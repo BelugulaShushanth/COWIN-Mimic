0a7f9d8153614bc847ebcc78376cddc5092a0bc4
Shushanth <sushanth179@gmail.com>
2021-11-09 20:04:49 +0530

    Added Dev Info

---
+++ js/devInfo.js
@@ -0,0 +1,4 @@
+var devinfo = document.getElementById('devInfo');
+	devinfo.addEventListener('click',()=>{
+    window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);
+  })
